0bd12f9ede62a689ab351e9f018539dd75175fb9
5castles <80058037+5castles@users.noreply.github.com>
2021-06-21 17:24:27 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hello, vanilla.");
+
+
+
+//str.indexOf(searchString(검색할 문자 또는 문자열)[, fromIndex])  
+
+//str.includes('something')); 인수로 전달한 문자열이 포함되어 있는지를 검사하고 결과를 불리언 값으로 반환한다. 두번째 인수는 옵션으로 검색할 위치를 나타내는 정수이다.
